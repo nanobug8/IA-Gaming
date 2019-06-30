fdeaf314d5bf9d801bebcfd2148106a8adf36f74
nanobug8 <52039127+nanobug8@users.noreply.github.com>
2019-06-30 00:29:58 -0300

    Delete Pieces.py

--- adversarial_search-master/adversarial_search/games/Pieces.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-
-class Pieces(ABC):
-        position =[]
-        HP = 0
-        moved = False
-        alcance = list() #lista de casillas donde cada pieza puede atacar.("la mira", lo que cad pieza ve)
